180ccb3e75c8e228b30e2ede907a85b98703c300
imranhAltersense <114974565+imranhAltersense@users.noreply.github.com>
2023-04-03 10:36:15 +0600

    error handled in register

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 //import modules
 import userRouter from "./routes/user.js";
@@ -10,12 +11,27 @@ import taskRouter from "./routes/task.js";
 // app
 export const app = express();
 // configuring environment
-config({path: "./config.env"});
+config({ path: "./config.env" });
 
 // use middleware
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Content-Type", "application/json;charset=UTF-8");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // all the routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
+
+// test api
+app.get("/", (req, res) => {
+  res.json({ message: "hello world" });
+});
--- controllers/user.js
+++ controllers/user.js
@@ -3,71 +3,77 @@ import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body;
-    //check if user already exists
+  console.log("i am called", req.body);
+  const { name, email, password } = req.body;
+  //check if user already exists
+  try {
     let user = await User.findOne({ email });
     if (user)
-        return res.status(404).json({
-            success: false,
-            message: "User already exist",
-        });
+      return res.status(404).json({
+        success: false,
+        message: "User already exist",
+      });
     // hashing password
     const hashedPassword = await bcrypt.hash(password, 10);
     //creating user
     user = await User.create({
-        name,
-        email,
-        password: hashedPassword,
+      name,
+      email,
+      password: hashedPassword,
     });
     // generating json web token
     sendCookie(user, res, "Registered successfully", 201);
+    // res.json({
+    //   success: true,
+    //   message: user,
+    // });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      error: "error",
+    });
+  }
 };
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    //first check if email exists
-    const user = await User.findOne({ email }).select("+password");
-    if (!user)
-        return res.status(404).json({
-            success: false,
-            message: "Invalid email or password",
-        });
-    //compare password
-    const isMatched = await bcrypt.compare(password, user.password);
-    if (!isMatched)
-        return res.status(404).json({
-            success: false,
-            message: "Invalid email or password",
-        });
-    //sending cookie with login
-    sendCookie(user, res, `Welcome back, ${user.name}`, 200);
+  const { email, password } = req.body;
+  //first check if email exists
+  const user = await User.findOne({ email }).select("+password");
+  if (!user)
+    return res.status(404).json({
+      success: false,
+      message: "Invalid email or password",
+    });
+  //compare password
+  const isMatched = await bcrypt.compare(password, user.password);
+  if (!isMatched)
+    return res.status(404).json({
+      success: false,
+      message: "Invalid email or password",
+    });
+  //sending cookie with login
+  sendCookie(user, res, `Welcome back, ${user.name}`, 200);
 };
 
 const logout = (req, res) => {
-    // setting cookie null
-    res.status(200)
-        .cookie("token", null, {
-            expires: new Date(Date.now()),
-        })
-        .json({
-            success: true,
-            message: "logout success",
-        });
-};
-
-
-const getUserProfile = async (req, res) => {
-    const user = req.user;
-    res.status(200).json({
-        success: true,
-        user,
+  // setting cookie null
+  res
+    .status(200)
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "logout success",
     });
 };
 
-
-export {
-    register,
-    login,
-    getUserProfile,
-    logout,
+const getUserProfile = async (req, res) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  });
 };
+
+export { register, login, getUserProfile, logout };
